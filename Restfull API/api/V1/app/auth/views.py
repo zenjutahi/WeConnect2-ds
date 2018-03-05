a48e342ac724cb07512429d52b18b09d5d7c5129
zenjutahi <zenjutahi@gmail.com>
2018-03-05 05:20:37 +0300

    Created user register and tested it on postman[finishes#155688213]

--- Restfull API/api/V1/app/auth/views.py
+++ Restfull API/api/V1/app/auth/views.py
@@ -7,15 +7,52 @@ from ..models import User
 
 
 # Set var to check user login status
+global logged_in
+logged_in = False
 
 @auth.route('/register', methods=['GET', 'POST'])
 def register():
     data = request.get_json()
+    # Check if email already registred
+    users_dict = User.users.items()
+    existing_user = {k:v for k, v in users_dict if data['email'] in v['email']}
+    if existing_user:
+        return jsonify({'message':'This email is registered, login instead'})
 
+
+    # If email not registred, create user account
     new_user = User(email=data['email'], username=data['username'], password=data['password'])
     new_user.create_user()
 
-    return jsonify({'message' : 'New user created'})
+    for key, value in users_dict:     # user gets id, eg 3
+        if data['email'] in value['email']:
+            session['user_id'] = key
 
-    
+    return jsonify({'message' : 'New user Succesfully created'})
+
+
 #user_id=str(uuid.uuid4()),
+
+@auth.route('/login', methods=['GET', 'POST'])
+def login():
+    data = request.get_json()
+
+    user_dict = User.users.items()
+    existing_user = {k:v for k, v in user_dict if data['email'] in v['email']}
+    if existing_user:
+        valid_user = [v for v in existing_user.values() if check_password_hash(v
+                        ['password'], data['password'])]
+        if valid_user:
+            global logged_in
+            logged_in = True
+
+            for key, value in users_dict:     # user gets id, eg 3
+                if data['email'] in value['email']:
+                    session['user_id'] = key
+            return jsonify({'message' : 'User valid succesfully logged in'})
+
+        else:
+            return jsonify({'mesage': 'Wrong password'})
+
+    else:
+        return jsonify({'message': 'Not registered user'})
